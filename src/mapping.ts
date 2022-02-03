c708fb969f9fa04a2a85fcea982e9355eacb3091
Federico Gomez <fedegomezdev@gmail.com>
2022-02-03 11:36:10 -0300

    start with compute balance

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,213 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import {
+  Transfer as TransferUsdcEvent,
+  USDC as UsdcContract,
+} from "../generated/usdc/USDC";
+import {
+  Transfer as TransferWbtcEvent,
+  WBTC as WbtcContract,
+} from "../generated/wbtc/WBTC";
+import {
+  Transfer as TransferWethEvent,
+  WETH as WethContract,
+} from "../generated/weth/WETH";
+import { Token, User, Balance } from "../generated/schema";
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+import { BigInt } from "@graphprotocol/graph-ts";
+
+export function handleUsdcTransfer(event: TransferUsdcEvent): void {
+  const tokenContract = UsdcContract.bind(event.address);
+  let userFrom = User.load(event.params.from.toHexString());
+  if (!userFrom) {
+    userFrom = new User(event.params.from.toHexString());
+    userFrom.save();
+  }
+  let userTo = User.load(event.params.to.toHexString());
+  if (!userTo) {
+    userTo = new User(event.params.to.toHexString());
+    userTo.save();
+  }
+  let token = Token.load(event.address.toHexString());
+  if (!token) {
+    token = new Token(event.address.toHexString());
+    token.name = tokenContract.name();
+    token.symbol = tokenContract.symbol();
+    token.totalSupply = tokenContract.totalSupply();
+    token.save();
+  }
+  let balanceFrom = Balance.load(
+    event.params.from.toHexString() + event.address.toHexString()
+  );
+  if (!balanceFrom) {
+    balanceFrom = new Balance(
+      event.params.from.toHexString() + event.address.toHexString()
+    );
+    balanceFrom.user = event.params.from.toHexString();
+    balanceFrom.token = event.address.toHexString();
+  }
+  balanceFrom.balance = tokenContract.balanceOf(event.params.from);
+  balanceFrom.save();
+
+  let balanceTo = Balance.load(
+    event.params.to.toHexString() + event.address.toHexString()
+  );
+  if (!balanceTo) {
+    balanceTo = new Balance(
+      event.params.to.toHexString() + event.address.toHexString()
+    );
+    balanceTo.user = event.params.to.toHexString();
+    balanceTo.token = event.address.toHexString();
+  }
+  balanceTo.balance = tokenContract.balanceOf(event.params.to);
+  balanceTo.save();
 }
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
+export function handleWbtcTransfer(event: TransferWbtcEvent): void {
+  const tokenContract = WbtcContract.bind(event.address);
+  let userFrom = User.load(event.params.from.toHexString());
+  if (!userFrom) {
+    userFrom = new User(event.params.from.toHexString());
+    userFrom.save();
   }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+  let userTo = User.load(event.params.to.toHexString());
+  if (!userTo) {
+    userTo = new User(event.params.to.toHexString());
+    userTo.save();
+  }
+  let token = Token.load(event.address.toHexString());
+  if (!token) {
+    token = new Token(event.address.toHexString());
+    token.name = tokenContract.name();
+    token.symbol = tokenContract.symbol();
+    token.totalSupply = tokenContract.totalSupply();
+    token.save();
+  }
+  let balanceFrom = Balance.load(
+    event.params.from.toHexString() + event.address.toHexString()
+  );
+  if (!balanceFrom) {
+    balanceFrom = new Balance(
+      event.params.from.toHexString() + event.address.toHexString()
+    );
+    balanceFrom.user = event.params.from.toHexString();
+    balanceFrom.token = event.address.toHexString();
+  }
+  balanceFrom.balance = tokenContract.balanceOf(event.params.from);
+  balanceFrom.save();
+
+  let balanceTo = Balance.load(
+    event.params.to.toHexString() + event.address.toHexString()
+  );
+  if (!balanceTo) {
+    balanceTo = new Balance(
+      event.params.to.toHexString() + event.address.toHexString()
+    );
+    balanceTo.user = event.params.to.toHexString();
+    balanceTo.token = event.address.toHexString();
+  }
+  balanceTo.balance = tokenContract.balanceOf(event.params.to);
+  balanceTo.save();
+}
+
+export function handleWethTransfer(event: TransferWethEvent): void {
+  const tokenContract = WethContract.bind(event.address);
+  let userFrom = User.load(event.params.from.toHexString());
+  if (!userFrom) {
+    userFrom = new User(event.params.from.toHexString());
+    userFrom.save();
+  }
+  let userTo = User.load(event.params.to.toHexString());
+  if (!userTo) {
+    userTo = new User(event.params.to.toHexString());
+    userTo.save();
+  }
+  let token = Token.load(event.address.toHexString());
+  if (!token) {
+    token = new Token(event.address.toHexString());
+    token.name = tokenContract.name();
+    token.symbol = tokenContract.symbol();
+    token.totalSupply = tokenContract.totalSupply();
+    token.save();
+  }
+  let balanceFrom = Balance.load(
+    event.params.from.toHexString() + event.address.toHexString()
+  );
+  if (!balanceFrom) {
+    balanceFrom = new Balance(
+      event.params.from.toHexString() + event.address.toHexString()
+    );
+    balanceFrom.user = event.params.from.toHexString();
+    balanceFrom.token = event.address.toHexString();
+  }
+  balanceFrom.balance = tokenContract.balanceOf(event.params.from);
+  balanceFrom.save();
+
+  let balanceTo = Balance.load(
+    event.params.to.toHexString() + event.address.toHexString()
+  );
+  if (!balanceTo) {
+    balanceTo = new Balance(
+      event.params.to.toHexString() + event.address.toHexString()
+    );
+    balanceTo.user = event.params.to.toHexString();
+    balanceTo.token = event.address.toHexString();
+  }
+  balanceTo.balance = tokenContract.balanceOf(event.params.to);
+  balanceTo.save();
+}
+
+export function handleAllTransfers(event: TransferWethEvent): void {
+  //User From
+  let userFrom = User.load(event.params.from.toHexString());
+  if (!userFrom) {
+    userFrom = new User(event.params.from.toHexString());
+    userFrom.save();
+  }
+
+  //User To
+  let userTo = User.load(event.params.to.toHexString());
+  if (!userTo) {
+    userTo = new User(event.params.to.toHexString());
+    userTo.save();
+  }
+
+  // Token
+  let token = Token.load(event.address.toHexString());
+  if (!token) {
+    const tokenContract = WethContract.bind(event.address);
+    token = new Token(event.address.toHexString());
+    token.name = tokenContract.name();
+    token.symbol = tokenContract.symbol();
+    token.totalSupply = tokenContract.totalSupply();
+    token.save();
+  }
+
+  // User Balance From
+  let balanceFrom = Balance.load(
+    event.params.from.toHexString() + event.address.toHexString()
+  );
+  if (!balanceFrom) {
+    balanceFrom = new Balance(
+      event.params.from.toHexString() + event.address.toHexString()
+    );
+    balanceFrom.user = event.params.from.toHexString();
+    balanceFrom.token = event.address.toHexString();
+    balanceFrom.balance = BigInt.fromI32(0); //event.params.value;
+  }
+  balanceFrom.balance = balanceFrom.balance.minus(event.params.value);
+  balanceFrom.save();
+
+  // User Balance To
+  let balanceTo = Balance.load(
+    event.params.to.toHexString() + event.address.toHexString()
+  );
+  if (!balanceTo) {
+    balanceTo = new Balance(
+      event.params.to.toHexString() + event.address.toHexString()
+    );
+    balanceTo.user = event.params.to.toHexString();
+    balanceTo.token = event.address.toHexString();
+    balanceTo.balance = BigInt.fromI32(0); //BigInt(0);
+  }
+  balanceTo.balance = balanceTo.balance.plus(event.params.value);
+  balanceTo.save();
 }
